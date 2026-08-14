8037ce10723f2693e1151a335695fa4ba94922df
LearnEHKing <learnehking@gmail.com>
2026-08-14 21:39:40 +0530

    Added Add new member page.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user, login_user , login_required, logout_user
 from werkzeug.security import generate_password_hash,check_password_hash
-from models import GymOwner,db
+from models import GymOwner,db, Member, Payment
 import config 
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "ppp"
@@ -24,10 +25,78 @@ def load_user(user_id):
 
 @app.route("/")
 def index():
-  
   if current_user.is_authenticated:
     return render_template("home.html")
   return render_template("index.html")
+  
+@app.route("/add_member", methods=["GET", "POST"])
+@login_required
+def add_member():
+    if request.method == "POST":
+      try:
+        
+        
+        # Member details
+        name = request.form["name"]
+        phone = request.form["phone"]
+        address = request.form["address"]
+        notes = request.form["notes"]
+        join_date = datetime.strptime(
+            request.form["join_date"], "%Y-%m-%d"
+        ).date()
+
+        # Payment details
+        plan = request.form["plan"]
+        fee = int(request.form["fee"])
+        payment_status = request.form["payment_status"]
+
+        # Create member
+        member = Member(
+            owner_id=current_user.id,
+            name=name,
+            phone=phone,
+            address=address,
+            join_date=join_date,
+            notes=notes
+        )
+
+        db.session.add(member)
+        db.session.flush()  # Gives member.id before commit
+
+        # Create first payment
+        payment = Payment(
+            member_id=member.id,
+            plan_name=plan,
+            amount=fee,
+            amount_paid=fee if payment_status == "Paid" else 0,
+            status=payment_status,
+            payment_date=join_date,
+          start_date=join_date,
+          expiry_date=join_date,
+          duration_days=6
+        )
+
+        db.session.add(payment)
+        db.session.commit()
+        return redirect("/")
+      except Exception as e:
+        print("\n\n", e , "\n\n")
+    return render_template("add_member.html")
+
+@app.route("/attendance",methods=['GET','POST'])
+@login_required
+def attendance():
+  return render_template("attendance.html")
+
+@app.route("/manage_payment",methods=['GET','POST'])
+@login_required
+def manage_payment():
+  return render_template("manage_payment.html")
+
+@app.route("/stats",methods=['GET','POST'])
+@login_required
+def stats():
+  return render_template("stats.html")
   
   
   
@@ -58,5 +127,7 @@ def logout():
 
     
 if __name__=="__main__":
+  with app.app_context():
+    db.create_all()
   app.run()
   
--- models.py
+++ models.py
@@ -15,12 +15,60 @@ class GymOwner( UserMixin, db.Model):
 
     members = db.relationship("Member",backref="owner", lazy=True)
 
-"""class Members( UserMixin, db.Model):
-    id = db.Column(db.Integer , primary_key=True)
-    username  = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash  = db.Column(db.String(255),nullable=False)
-    name = db.Column(db.String(100),nullable=False)
-    phone  = db.Column(db.String(15),nullable=False)
-    join_date  = db.Column(db.Date, default = date.today)
-    payment_due_date = db.Column(db.Date)
-    plan  = db.Column(db.String(20),nullable=False)"""
+class Member(db.Model):
+    __tablename__ = "member"
+
+    __table_args__ = (
+        db.UniqueConstraint(
+            "owner_id",
+            "name",
+            name="uq_owner_member_name"
+        ),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    owner_id = db.Column(
+        db.Integer,
+        db.ForeignKey("gym_owner.id"),
+        nullable=False
+    )
+
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(150), nullable=False)
+    phone = db.Column(db.String(15))
+    join_date = db.Column(db.Date)
+    notes = db.Column(db.Text)
+    active = db.Column(db.Boolean, default=True)
+
+
+class Payment(db.Model):
+    __tablename__ = "payment"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    member_id = db.Column(
+        db.Integer,
+        db.ForeignKey("member.id"),
+        nullable=False
+    )
+
+    # Membership
+    plan_name = db.Column(db.String(50), nullable=False)
+    duration_days = db.Column(db.Integer, nullable=False)
+
+    # Money
+    amount = db.Column(db.Integer, nullable=False)
+    amount_paid = db.Column(db.Integer, default=0)
+
+    # Dates
+    payment_date = db.Column(db.Date, nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    expiry_date = db.Column(db.Date, nullable=False)
+
+    # Paid / Partial / Unpaid
+    status = db.Column(db.String(20), nullable=False)
+
+    remarks = db.Column(db.Text)
+
+    member = db.relationship("Member", backref="payments")
