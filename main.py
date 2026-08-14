b7006a12ac3a387e84f39d692506484daba80740
LearEHKing <learnehking@gmail.com>
2026-08-14 16:49:34 +0530

    Added gym owner database and login

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 from flask import Flask, render_template, redirect, url_for, request
-#from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user, login_user , login_required, logout_user
+from werkzeug.security import generate_password_hash,check_password_hash
+from models import GymOwner,db
 
 app = Flask(__name__)
 app.secret_key = "ppp"
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gym.db"
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gym.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-#db = SQLAlchemy(app)
+db.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+   return GymOwner.query.get(int(user_id))
 
 @app.route("/")
 def index():
+  
+  if current_user.is_authenticated:
+    return render_template("home.html")
   return render_template("index.html")
   
+  
+  
+
 @app.route("/login",methods=['GET','POST'])
 def login():
-  if request.method == "GET":
-      return render_template("login.html")
+  if current_user.is_authenticated:
+    return redirect("/")
 
-    # POST request
-  username = request.form["username"]
-  password = request.form["password"]
-
-    # Check database here
-  if username == "admin" and password == "1234":
-      session["user_id"] = 1
+  error=None
+  if request.method == "POST":
+      # POST request
+    username = request.form["username"]
+    password = request.form["password"]
+    owner = GymOwner.query.filter_by(username=username).first()
+    if owner and check_password_hash(owner.password_hash,password):
+      login_user(owner)
       return redirect("/")
-  return render_template("login.html", error="Invalid username or password")
+    else :
+      error="Invalid username or password."
+  return render_template("login.html",error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect("/")
 
     
-
 if __name__=="__main__":
+
   app.run()
+  
---
+++ models.py
@@ -0,0 +1,14 @@
+from datetime import date
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+class GymOwner( UserMixin, db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    username  = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash  = db.Column(db.String(255),nullable=False)
+    name = db.Column(db.String(100),nullable=False)
+    phone  = db.Column(db.String(15),nullable=False)
+    join_date  = db.Column(db.Date, default = date.today)
+    payment_due_date = db.Column(db.Date)
+    plan  = db.Column(db.String(20),nullable=False)
